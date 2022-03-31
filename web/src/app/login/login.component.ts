f7e309da2ddab24bf5ebdd9f8d0360d75b390a1b
Thomas King <tking@shift3tech.com>
2022-03-31 12:03:44 -0700

    adding simple login form

--- web/src/app/login/login.component.ts
+++ web/src/app/login/login.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { AuthService } from '../api/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  loginForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+  ) { }
 
-  ngOnInit(): void {
+  login(): void {
+    this.auth.login(this.loginForm.value).subscribe(console.log);
   }
 
 }
