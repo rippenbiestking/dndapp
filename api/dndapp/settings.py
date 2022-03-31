73257442bd916cfd4e571b168a4fe9fee64d41ad
Thomas King <tking@shift3tech.com>
2022-03-31 11:14:13 -0700

    adding endpoints to api for token auth

--- api/dndapp/settings.py
+++ api/dndapp/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
     'user',
 ]
 
@@ -122,6 +124,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
 
 try : 
     from .localsettings import *
