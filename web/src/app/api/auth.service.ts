6e0fbe339ef3e778d03f35aa733ca770d97f04bb
Thomas King <tking@shift3tech.com>
2022-03-31 19:35:25 -0700

    saving to local storage

--- web/src/app/api/auth.service.ts
+++ web/src/app/api/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpRequest } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
+import { StorageService } from '../storage.service';
 
 export type TokenResponse = {
   access: string,
@@ -16,11 +17,36 @@ export type TokenRequest = {
   providedIn: 'root'
 })
 export class AuthService {
-  token?: TokenResponse;
+  private _token?: TokenResponse;
+
+  set token(t: TokenResponse | undefined) {
+    if (t) {
+      this.storage.set('access', t.access);
+      this.storage.set('refresh', t.refresh);
+    } else {
+      this.storage.clear('access');
+      this.storage.clear('refresh');
+    }
+    this._token = t;
+  }
+
+  get token(): TokenResponse | undefined {
+    return this._token;
+  }
 
   constructor(
+    private storage: StorageService,
     private http: HttpClient,
-  ) { }
+  ) {
+    const access = this.storage.get('access');
+    const refresh = this.storage.get('refresh');
+    if (access && refresh) {
+      this._token = {
+        access,
+        refresh,
+      }
+    }
+  }
 
   login(tokenRequest: TokenRequest): Observable<TokenResponse> {
     return this.http.post<TokenResponse>('/api/token/', tokenRequest).pipe(
