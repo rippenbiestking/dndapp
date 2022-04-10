9c6af59b454b4f145876a356a9b7786f2c8f74a6
Thomas King <tking@shift3tech.com>
2022-04-09 18:13:48 -0700

    made some stuff work

--- api/dndapp/urls.py
+++ api/dndapp/urls.py
@@ -33,8 +33,8 @@ from user.views import register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls))
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/register/', register, name='register'),
+    path('api/', include(router.urls)),
 ]
